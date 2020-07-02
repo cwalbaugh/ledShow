a7dee9f35b6cc7d890911934135592e9982913fe
Christian Albaugh <christianalbaugh@gmail.com>
2020-07-01 21:27:59 -0500

    Update show.py

--- show.py
+++ show.py
@@ -6,3 +6,11 @@ import neopixel
 pixels = neopixel.NeoPixel(board.D18, 30)
 
 pixels[0] = (255, 0, 0)
+sleep(10)
+pixels[0] = (0, 0, 0)
+
+for x in range(0, 9):
+    pixels[x] = (255, 0, 0)
+    sleep(1)
+    
+    
