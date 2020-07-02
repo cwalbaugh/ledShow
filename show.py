ad55e04817f660848314c48fb2678b72865ae78e
Christian Albaugh <christianalbaugh@gmail.com>
2020-07-01 21:34:03 -0500

    Update show.py

--- show.py
+++ show.py
@@ -5,12 +5,20 @@ import board
 import neopixel
 pixels = neopixel.NeoPixel(board.D18, 30)
 
-pixels[0] = (255, 0, 0)
-sleep(10)
-pixels[0] = (0, 0, 0)
 
-for x in range(0, 9):
-    pixels[x] = (255, 0, 0)
-    sleep(1)
+print("select show:\n")
+print("1:show one\n")
+
+show = input("Please enter a string:\n")
+
+if show == "1"
+    print("running show 1")
+    pixels[0] = (255, 0, 0)
+    sleep(10)
+    pixels[0] = (0, 0, 0)
+
+    for x in range(0, 9):
+        pixels[x] = (255, 0, 0)
+        sleep(1)
     
     
