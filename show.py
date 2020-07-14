3c0e8149bb9ed67f2286fe3b57fcb7e546d2358e
cwalbaugh <christianalbaugh@gmail.com>
2020-07-13 21:22:55 -0500

    more testing

--- show.py
+++ show.py
@@ -1,24 +1,11 @@
-#!/usr/bin/env python3
-print("Running Show")
-
 import board
 import neopixel
-pixels = neopixel.NeoPixel(board.D18, 30)
+#pixels = neopixel.NeoPixel(board.D18, 300, brightness=0.2, auto_write=True, pixel_order=neopixel.RGB)
+#pixels = neopixel.NeoPixel(board.D18, 300, brightness=0.01)
+pixels = neopixel.NeoPixel(board.D18, 300)
 
-
-print("select show:\n")
-print("1:show one\n")
-
-show = input("Please enter a string:\n")
-
-if show == "1"
-    print("running show 1")
-    pixels[0] = (255, 0, 0)
-    sleep(10)
-    pixels[0] = (0, 0, 0)
-
-    for x in range(0, 9):
-        pixels[x] = (255, 0, 0)
-        sleep(1)
-    
-    
+#pixels[0] = (0, 0, 0)
+pixels[0] = (255, 255, 255)
+#pixels.fill((238, 130, 238))
+#pixels.fill((255, 255, 255))
+#pixels.fill((255, 0, 0))
